9ba3e9630036fb365bd33b2bb726206a3ed7e13d
Antoine <antoine.crzt@gmail.com>
2018-12-06 20:15:15 +0100

    Pivot table between Missions and Stuffs migration

---
+++ database/migrations/2018_12_06_191220_create_mission_stuff_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMissionStuffTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('mission_stuff', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('mission_id')->unsigned();
+            $table->integer('stuff_id')->unsigned();
+
+            $table->foreign('mission_id')->references('id')->on('mission')->onDelete('cascade');
+            $table->foreign('stuff_id')->references('id')->on('stuff');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('mission_stuff', function (Blueprint $table) {
+            Schema::drofIfExists('mission_stuff');
+        });
+    }
+}
