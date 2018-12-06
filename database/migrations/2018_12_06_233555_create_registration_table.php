48ea8d88d364a207f0560cc00f41a9e2e5227db2
Antoine <antoine.crzt@gmail.com>
2018-12-07 00:53:34 +0100

    Tous les trucs de Bastien

---
+++ database/migrations/2018_12_06_233555_create_registration_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegistrationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registration', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('email');
+            $table->string('token');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registration');
+    }
+}
