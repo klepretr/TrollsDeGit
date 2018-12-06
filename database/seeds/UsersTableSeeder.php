1aa6ef42077ca725c7e3bf9a1cf3bfe5cb9401ed
Antoine <antoine.crzt@gmail.com>
2018-12-06 22:34:17 +0100

    User Seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	for(i=0; i<10;i++) {
+    		\DB::table('users')->insert([
+            'name' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+            'role'=>0,
+            'age'=>12,
+            'gender'=>1,
+            'firstname'=>'John'.i, 
+            'lastname'=>'Doe'.i,
+            'phone'=>'+33666666',
+        ]);
+    	}
+        
+    }
+}
