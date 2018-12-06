3644a570be14aab9fd3514ee4ee15485bef82576
Antoine <antoine.crzt@gmail.com>
2018-12-06 22:41:45 +0100

    Database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
+        $this->call(MissionsTableSeeder::class);
+        $this->call(StuffsTableSeeder::class);
+        $this->call(UsersTasksTableSeeder::class);
+        $this->call(MissionsTasksTableSeeder::class);
+
     }
 }
