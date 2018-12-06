30d0ec5bc653480ff64859fea4e4f7e7f29c9bd4
Antoine <antoine.crzt@gmail.com>
2018-12-06 22:02:52 +0100

    MissionsTasks migration

---
+++ database/migrations/2018_12_06_210212_create_missions_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMissionsTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('missions_tasks', function (Blueprint $table) {
+           table->increments('id');
+            $table->text('description');
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->integer('mission_id')->unsigned()->references('id')->on('missions')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('missions_tasks');
+    }
+}
