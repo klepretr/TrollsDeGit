9452e20e9172450c915b241bb58c3d954d0a0293
Antoine <antoine.crzt@gmail.com>
2018-12-06 21:45:01 +0100

    Files model (and linked relationships

--- app/Models/File.php
+++ app/Models/File.php
@@ -4,15 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Report extends Model
+class File extends Model
 {
-     /**
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'mission_id', 'content'
+        'url', 'title', 'uploaded_by'
     ];
 
     /**
@@ -21,15 +21,17 @@ class Report extends Model
      * @var array
      */
     protected $hidden = [
+        
     ];
 
-    public function files()
+    public function report()
     {
-        return $this->hasMany(App\Models\Report::class);
+    	return $this->belongsTo(App\Models\Report::class);
     }
 
-    public function mission()
+    public function user()
     {
-    	return $this->belongsTo(App\Models\Mission::class);
+    	return $this->belongsTo(App\Models\User::class);
     }
+
 }
