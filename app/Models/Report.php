c78fd7500adec1e3324fd8ae0bf494c331f2c10b
Antoine <antoine.crzt@gmail.com>
2018-12-06 21:39:51 +0100

    Report model

---
+++ app/Models/Report.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Report extends Model
+{
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'mission_id', 'content'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    public function files()
+    {
+        return $this->belongsToMany(App\Models\Report::class);
+    }
+
+    public function mission()
+    {
+    	return $this->belongsTo(App\Models\Mission::class);
+    }
+}
