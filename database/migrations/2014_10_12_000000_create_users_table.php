9652c183407e272fef3a4b888e08641426178e13
vagrant <vagrant@homestead>
2018-12-06 18:59:50 +0000

    Users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsigned()->integer('age');
+            $table->string('firstname')->unique();
+            $table->string('lastname');
+            $table->unsigned()->integer('role');
+            $table->string('phone_number', 36)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
